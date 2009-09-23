86f1e5b7a9ff56cf6a9efa4babcae426d616d36d
Jeremy Leibs <jleibs@gmail.com>
2009-09-23 03:42:18 +0000

    Fixing documentation in laser_geometry.
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/laser_pipeline/trunk@24354 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- include/laser_geometry/laser_geometry.h
+++ include/laser_geometry/laser_geometry.h
@@ -144,8 +144,7 @@ namespace laser_geometry
        * \param scan_in The input laser scan
        * \param cloud_out The output point cloud
        * \param tf a tf::Transformer object to use to perform the
-       *   transform applied which is more limiting than max_range in
-       *   the scan.
+       *   transform
        * \param channel_option An OR'd set of channels to include.
        *   Options include: channel_option::Default,
        *   channel_option::Intensity, channel_option::Index,
