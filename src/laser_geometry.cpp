4c6e81b8279fba3c12fcc6f4cfc6b301c250f854
Jeremy Leibs <jleibs@gmail.com>
2010-01-09 04:52:34 +0000

    Applying patch for #3550
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/laser_pipeline/trunk@26796 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- src/laser_geometry.cpp
+++ src/laser_geometry.cpp
@@ -252,9 +252,9 @@ const boost::numeric::ublas::matrix<double>& LaserProjection::getUnitVectors_(do
     ros::Time start_time = scan_in.header.stamp ;
     ros::Time end_time   = scan_in.header.stamp + ros::Duration().fromSec(scan_in.get_ranges_size()*scan_in.time_increment) ;
 
-    tf::Stamped<tf::Transform> start_transform ;
-    tf::Stamped<tf::Transform> end_transform ;
-    tf::Stamped<tf::Transform> cur_transform ;
+    tf::StampedTransform start_transform ;
+    tf::StampedTransform end_transform ;
+    tf::StampedTransform cur_transform ;
 
     tf.lookupTransform(target_frame, scan_in.header.frame_id, start_time, start_transform) ;
     tf.lookupTransform(target_frame, scan_in.header.frame_id, end_time, end_transform) ;
