f8e27a00c62b91ad6da6818f38a8e57158143900
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-07-12 17:09:44 +0200

    feedback
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- src/laser_geometry/__init__.py
+++ src/laser_geometry/__init__.py
@@ -1,5 +1,6 @@
-# Copyright (c) 2024, Open Source Robotics Foundation, Inc.
+# Copyright (c) 2014, Enrique Fernandez
 # All rights reserved.
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
--- test/projection_test.py
+++ test/projection_test.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
-# Copyright (c) 2024, Open Source Robotics Foundation, Inc.
+# Copyright (c) 2014, Enrique Fernandez
 # All rights reserved.
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
